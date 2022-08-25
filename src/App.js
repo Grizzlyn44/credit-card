5f48bced6c7de1eab18544afba1982360c430ea0
Martin Pražák <grizzlyn44@gmail.com>
2022-08-25 16:52:49 +0200

    animations

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ const App = () => {
   return (
     <div className="App">
       <div className="main-container">
-        <span className="bg-dot left" />
-        <span className="bg-dot right" />
         <div className="inner">
+          <div className="bg-dot-c">
+            <span className="bg-dot left" />
+            <span className="bg-dot right" />
+          </div>
           <div className="heading-text">
             Please enter your credit card details below
           </div>
