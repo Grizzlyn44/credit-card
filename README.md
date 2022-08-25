804ad94697db12cb3ce6c35dc9d2cae3eca74d2a
Martin Pražák <grizzlyn44@gmail.com>
2022-08-25 19:33:02 +0200

    card number input update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Typescirpt
+
+ANO, SCHVÁLNĚ TO NENÍ V `TYPESCRIPTU` .
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/Card/CardNumber.js
+++ src/components/Card/CardNumber.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const formatNumber = (valueFromInput) => {
-  const valueToUse = valueFromInput.trim();
+  // Should be better :)
+  const valueToUse = valueFromInput?.replace(/\s+/g, "");
+  if (valueToUse.length > 16) return valueFromInput.slice(0, -1);
   let stringFormatted = "";
   for (let x = 0; x < valueToUse.length; x++) {
-    if ((x + 1) % 4 === 0 && x !== 0) {
-      stringFormatted = stringFormatted + `${valueToUse[x]} `;
+    if (x % 4 === 0 && x !== 0) {
+      stringFormatted = stringFormatted + ` ${valueToUse[x]}`;
     } else {
       stringFormatted = stringFormatted + valueToUse[x];
     }
@@ -14,43 +16,23 @@ const formatNumber = (valueFromInput) => {
   return stringFormatted;
 };
 
-const generateInputs = () => {
-  let inputs = [];
-
-  for (let x = 0; x < 16; x++) {
-    inputs.push(<input className="c-n-i-s" />);
-  }
-
-  return inputs;
-};
-
 const CardNumber = (props) => {
   const value = props.value;
   const setCardNumber = props.setCardNumber;
-
-  // const formattedString = formatNumber(value);
-
-  const html = value?.split(" ").map((e, index) => (
-    <span key={`digit-${index}`} className="f-digit">
-      {e}
-    </span>
-  ));
 
   return (
     <div className="card-number">
       <div className="heading">Card Number</div>
       <div className="content">
-        {generateInputs()}
-        {/* <input
+        <input
           value={value}
           className="c-n-i"
           onChange={(e) => {
-            const formattedString = formatNumber(e.target.value);
-            setCardNumber(formattedString);
-            // console.log("change", e.target.value);
+            const inputValue = e.target.value;
+            const formattedValue = formatNumber(inputValue);
+            setCardNumber(formattedValue);
           }}
-        /> */}
-        {/* {html} */}
+        />
       </div>
     </div>
   );
